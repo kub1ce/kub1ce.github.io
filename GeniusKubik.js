4340ceffc01733453d4da341583712f91708e35e
Kubice <83603203+1ce-kube@users.noreply.github.com>
2022-05-09 22:18:10 +0500

    Add files via upload

--- GeniusKubik.js
+++ GeniusKubik.js
@@ -19,5 +19,31 @@ function change_naving(value){
     document.querySelector('#news').style.display = 'none';
     document.querySelector('#info').style.display = 'none';
     document.querySelector('#' + value).style.display = 'block';
+};
 
+function edit_background(value){
+    if(value == 'light'){
+        document.body.style.background = 'none';
+        document.body.style.backgroundColor = '#fff';
+        document.body.style.color = '#000';
+        n = document.querySelector('#main');
+        n.style.background = '#00000050';
+        n.style.border = '2px solid #000';
+        document.querySelector('#naving_nav').style.border = '2px black solid';
+    }else if(value == 'dark'){
+        document.body.style.background = 'none'
+        document.body.style.backgroundColor = '#1a1a1a';
+        document.body.style.color = '#fff';
+        n = document.querySelector('#main');
+        n.style.background = '#ffffff50';
+        n.style.border = '2px solid #fff';
+        document.querySelector('#naving_nav').style.border = '2px white solid';
+    }else{
+        document.body.style.background = 'linear-gradient(to top, #693ABB, #C93ED3)';
+        document.body.style.color = '#000';
+        n = document.querySelector('#main');
+        n.style.background = '#ffffff50';
+        n.style.border = '2px solid #fff';
+        document.querySelector('#naving_nav').style.border = '2px white solid';
+    }
 }
